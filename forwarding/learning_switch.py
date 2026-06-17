53836a6b986a5530470dbc4168c94f8bbe286eb2
Juan Manuel Becerra <jbecerra@whitestack.com>
2026-06-16 23:27:55 -0500

    Fix flows granularity

--- forwarding/learning_switch.py
+++ forwarding/learning_switch.py
@@ -1,7 +1,7 @@
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
-from ryu.lib.packet import ether_types
 from ryu.lib.packet import ipv4
+from ryu.lib.packet import ether_types
 from ryu.lib.packet import tcp
 from ryu.lib.packet import udp
 from ryu.lib.packet import icmp
@@ -80,24 +80,24 @@ class LearningSwitch:
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        dpid = datapath.id
-
-        self.mac_to_port.setdefault(dpid, {})
-
         in_port = msg.match["in_port"]
 
         pkt = packet.Packet(msg.data)
 
         eth = pkt.get_protocol(ethernet.ethernet)
 
-        if eth is None:
+        if not eth:
             return
 
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             return
 
-        src = eth.src
         dst = eth.dst
+        src = eth.src
+
+        dpid = datapath.id
+
+        self.mac_to_port.setdefault(dpid, {})
 
         self.mac_to_port[dpid][src] = in_port
 
@@ -110,73 +110,51 @@ class LearningSwitch:
             parser.OFPActionOutput(out_port)
         ]
 
-        if out_port == ofproto.OFPP_FLOOD:
-
-            data = None
-
-            if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-                data = msg.data
-
-            out = parser.OFPPacketOut(
-                datapath=datapath,
-                buffer_id=msg.buffer_id,
-                in_port=in_port,
-                actions=actions,
-                data=data
-            )
-
-            datapath.send_msg(out)
-            return
+        # ---------------------------------
+        # EXTRAER IPv4
+        # ---------------------------------
 
         ip_pkt = pkt.get_protocol(ipv4.ipv4)
 
-        if ip_pkt:
+        # ---------------------------------
+        # INSTALAR FLOW
+        # ---------------------------------
 
-            match_fields = {
-                "eth_type": 0x0800,
-                "ipv4_src": ip_pkt.src,
-                "ipv4_dst": ip_pkt.dst,
-                "ip_proto": ip_pkt.proto
-            }
+        if out_port != ofproto.OFPP_FLOOD:
 
-            tcp_pkt = pkt.get_protocol(tcp.tcp)
-            udp_pkt = pkt.get_protocol(udp.udp)
+            if ip_pkt:
 
-            if tcp_pkt:
+                match = parser.OFPMatch(
+                    eth_type=0x0800,
+                    ipv4_src=ip_pkt.src,
+                    ipv4_dst=ip_pkt.dst
+                )
 
-                match_fields["tcp_src"] = tcp_pkt.src_port
-                match_fields["tcp_dst"] = tcp_pkt.dst_port
+                self.add_flow(
+                    datapath,
+                    priority=10,
+                    match=match,
+                    actions=actions,
+                    idle_timeout=60,
+                    hard_timeout=0
+                )
 
-            elif udp_pkt:
+            else:
 
-                match_fields["udp_src"] = udp_pkt.src_port
-                match_fields["udp_dst"] = udp_pkt.dst_port
+                match = parser.OFPMatch(
+                    in_port=in_port,
+                    eth_src=src,
+                    eth_dst=dst
+                )
 
-            match = parser.OFPMatch(
-                **match_fields
-            )
-
-            print(
-                f"L3/L4 FLOW {ip_pkt.src} -> {ip_pkt.dst} proto={ip_pkt.proto}"
-            )
-
-        else:
-
-            match = parser.OFPMatch(
-                eth_type=0x0800,
-                ipv4_src=src_ip,
-                ipv4_dst=dst_ip,
-                ip_proto=ip_proto
-            )
-
-        self.add_flow(
-            datapath,
-            priority=10,
-            match=match,
-            actions=actions,
-            idle_timeout=60,
-            hard_timeout=0
-        )
+                self.add_flow(
+                    datapath,
+                    priority=1,
+                    match=match,
+                    actions=actions,
+                    idle_timeout=60,
+                    hard_timeout=0
+                )
 
         data = None
 
